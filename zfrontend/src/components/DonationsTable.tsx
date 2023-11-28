409f1ad8d4268eefef4f9e5f15eb46b415ef2d40
silectron <yiheng.liu.email@gmail.com>
2023-11-28 16:42:24 -0500

    init nextjs frontend

---
+++ zfrontend/src/components/DonationsTable.tsx
@@ -0,0 +1,14 @@
+export default function DonationsTable() {
+    return (
+        <table className="table-auto">
+            <thead>
+                <tr>
+                    <th className="px-4 py-2">Date</th>
+                    <th className="px-4 py-2">First Name</th>
+                    <th className="px-4 py-2">Last Name</th>
+                    <th className="px-4 py-2">Amount</th>
+                </tr>
+            </thead>
+        </table>
+    );
+}
